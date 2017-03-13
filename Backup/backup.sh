83c95deb77b5e2dd57b60f0d603c6394e68b4099
GeorgeWalker <gwalker@escapesystems.com>
2017-03-13 15:04:26 -0700

    HETS-79 backup script

---
+++ Backup/backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+BACKUP_DIR=/backups
+
+FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d`/"
+ 
+echo "Making backup directory in $FINAL_BACKUP_DIR"
+ 
+if ! mkdir -p $FINAL_BACKUP_DIR; then
+	echo "Cannot create backup directory in $FINAL_BACKUP_DIR." 1>&2
+	exit 1;
+fi;
+
+PGPASSWORD="$POSTGRESQL_PASSWORD"
+
+if ! pg_dump -Fp -h "$DATABASE_SERVICE_NAME" -U "$POSTGRESQL_USER" "$POSTGRESQL_DATABASE" | gzip > $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress; then
+			echo "[!!ERROR!!] Failed to produce plain backup database $DATABASE" 1>&2
+else
+	mv $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress $FINAL_BACKUP_DIR"$DATABASE".sql.gz
+fi
